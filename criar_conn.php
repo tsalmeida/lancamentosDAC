ee6d3a92b1e80e29665e06b1881454817566cac1
tsalmeida <tsilvaalmeida@gmail.com>
2020-10-15 14:00:20 -0300

    Create criar_conn.php

---
+++ criar_conn.php
@@ -0,0 +1,8 @@
+<?php
+	$servername = "localhost";
+	$username = "gestor";
+	$password = "ubique patriae memor";
+	$dbname = "lancamentosDAC";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	mysqli_set_charset($conn, "utf8");
+	?>
